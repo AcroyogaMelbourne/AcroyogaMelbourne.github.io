ba7f176b2276169b472894e166b88b996e387942
Gregory Clarke <greg.clarke@shopify.com>
2014-11-21 23:13:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@
  * Please share and forgive me for my horrile javascript skills :)
  */
 
-var title = 'Acroyoga Ottawa Event Calendar';
+var title = 'Acroyoga Melbourne Event Calendar';
 var tagline = 'Acrobatics with a yogic consciousness in a beautiful city';
-var instagram_hash_tag = 'AcroyogaOttawa';
+var instagram_hash_tag = 'AcroyogaMelbourne';
 var instagram_client_id = '4c0caa35855143e788ca790012daaa3b';
 var google_calendar_api_key = 'AIzaSyAPjGPP7lWZPN1o1eBOeQzi7TL2jl3t5Ac';
-var google_calendar = 'gho.st_19b0g4d2m46ta2l2gf8olt1k6o@group.calendar.google.com';
-var social_links = '<a href="https://www.facebook.com/groups/AcroyogaOttawa/">Facebook</a> - <a href="https://twitter.com/AcroyogaOttawa">Twitter</a> - <a href="http://www.pinterest.com/gregology/acroyoga-ottawa/">Pinterest</a>';
-var contact_name = 'Greg';
-var contact_email = 'greg@smileyom.com';
-var google_analytics = 'UA-47043442-2';
+var google_calendar = 'gho.st_lthf1sf40g8kkaq1k8a32j08tg@group.calendar.google.com';
+var social_links = '<a href="https://www.facebook.com/groups/152312374814747">Facebook</a>';
+var contact_name = 'Sally';
+var contact_email = 'Sally.omm@gmail.com';
+var google_analytics = 'UA-47043442-7';
